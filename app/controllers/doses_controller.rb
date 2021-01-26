867117b2c6809de432586f28ed158aa973d84b6d
Julian Harrington <jrayharrington@gmail.com>
2021-01-26 20:45:07 +1100

    updated routes to root path

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -19,6 +19,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+
     redirect_to cocktail_path(@dose.cocktail)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'cocktails#index'
+
   resources :cocktails do
     resources :doses, except: :destroy
   end
