b84df8d7a4382c9458fae6946dc4664622b0d4da
Julian Harrington <jrayharrington@gmail.com>
2021-01-26 17:07:28 +1100

    finish show and new page, create consistency between pages and buttons

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,6 +24,8 @@ class CocktailsController < ApplicationController
   def destroy
     @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
+
+    redirect_to cocktails_path
   end
 
   private
